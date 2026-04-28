75f79ca9195613c55ca19d868aafd2d57932876f
Pablox8 <pcarro90@gmail.com>
2026-04-28 11:17:47 +0200

    Updated __init__.py

--- ex_fuzzy_reg/__init__.py
+++ ex_fuzzy_reg/__init__.py
@@ -1,4 +1,56 @@
-from . import fuzzy_sets 
-from . import fuzzy_variable
-from . import rules_reg
-from . import rules_reg_utils
+# Public API of ex-fuzzy-reg
+#
+# Users should import from here, not from internal modules:
+#   from ex_fuzzy_reg import MamdaniFIS, BaseFuzzyRulesRegressor
+#   from ex_fuzzy_reg import FuzzyVariable, TriangularFS, FUZZY_SETS
+
+# Regressors (main entry points)
+from .regressors import MamdaniFIS, load_from_json
+from .evolutionary_fit_reg import BaseFuzzyRulesRegressor
+
+# Fuzzy sets
+from .fuzzy_sets import (
+    FUZZY_SETS,
+    FS,
+    TrapezoidalFS,
+    TriangularFS,
+    GaussianFS,
+)
+
+# Fuzzy variables
+from .fuzzy_variable import FuzzyVariable
+
+# Rule bases
+from .rules_reg import RuleBaseRegT1
+
+# Partition and rule generation utilities
+from .rules_reg_utils import (
+    generate_triangular_partitions,
+    generate_trapezoidal_partitions,
+    generate_rules,
+)
+
+__all__ = [
+    # Regressors
+    "MamdaniFIS",
+    "BaseFuzzyRulesRegressor",
+    "load_from_json",
+
+    # Fuzzy sets
+    "FUZZY_SETS",
+    "FS",
+    "TrapezoidalFS",
+    "TriangularFS",
+    "GaussianFS",
+
+    # Fuzzy variables
+    "FuzzyVariable",
+
+    # Rule bases
+    "RuleBaseRegT1",
+
+    # Utilities
+    "generate_triangular_partitions",
+    "generate_trapezoidal_partitions",
+    "generate_rules",
+]
